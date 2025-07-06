f2b05a5036ed93fbd30e01ffde0048f7c05d33d8
Jason <sanchezjason@me.com>
2025-07-06 15:47:06 -0600

    cricut/swiftui/shapes- created viewModel, loadButtons

---
+++ ShapesApp/ShapesApp/ViewModels/ShapesGridViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  ShapesGridViewModel.swift
+//  ShapesApp
+//
+//  Created by Jason Sanchez on 7/6/25.
+//
+
+import Foundation
+import SwiftUI
+
+class ShapesGridViewModel: ObservableObject {
+    @Published var buttons: [Buttons] = []
+    @Published var gridShapes: [GridShape] = []
+    @Published var errorMessage: String?
+    
+    private let gridSize: CGFloat = 50.0
+    
+    func loadButtons() async {
+        do {
+            let response = try await NetworkService().load(url: URLs.cricutURL)
+            self.buttons = response
+        } catch {
+            errorMessage = error.localizedDescription
+        }
+    }
+}
